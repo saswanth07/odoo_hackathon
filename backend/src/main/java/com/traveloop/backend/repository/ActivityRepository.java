132f4e3b74530c785eee5a882b6a70cabfbeb610
Saswanth <saswanth545@gmail.com>
2026-05-10 17:43:59 -0300

    Removed backend and frontend folders

--- backend/src/main/java/com/traveloop/backend/repository/ActivityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.traveloop.backend.repository;
-
-import com.traveloop.backend.entity.Activity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ActivityRepository extends JpaRepository<Activity, Long> {
-    List<Activity> findByStopId(Long stopId);
-}
--- backend/src/main/java/com/traveloop/backend/repository/TripNoteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.traveloop.backend.repository;
-
-import com.traveloop.backend.entity.TripNote;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface TripNoteRepository extends JpaRepository<TripNote, Long> {
-    List<TripNote> findByTripId(Long tripId);
-}
